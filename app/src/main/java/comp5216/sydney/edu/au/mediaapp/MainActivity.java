9068ee3f2f3a3d0d9dd14b296e43a37da5e66677
Bora <borabalos9@gmail.com>
2023-09-24 15:43:33 +1000

    New base

--- app/src/main/java/comp5216/sydney/edu/au/mediaapp/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/mediaapp/MainActivity.java
@@ -1,193 +1,252 @@
 package comp5216.sydney.edu.au.mediaapp;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaRecorder;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraInfoUnavailableException;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
-
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.firebase.FirebaseApp;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
-import java.util.concurrent.ExecutionException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private static final int PERMISSIONS_REQUEST_CODE = 10;
-    private static final String[] CAMERA_STORAGE_PERMISSIONS = {
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+    //request codes
+    private static final int MY_PERMISSIONS_REQUEST_OPEN_CAMERA = 101;
+    private static final int MY_PERMISSIONS_REQUEST_READ_PHOTOS = 102;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_VIDEO = 103;
+    private static final int MY_PERMISSIONS_REQUEST_READ_VIDEOS = 104;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 105;
+    public final String APP_TAG = "MobileComputingTutorial";
+    public String photoFileName = "photo.jpg";
+    public String videoFileName = "video.mp4";
+    public String audioFileName = "audio.3gp";
+    MarshmallowPermission marshmallowPermission = new MarshmallowPermission(this);
 
-    private static final String[] LOCATION_PERMISSIONS = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-    };
-
-    private PreviewView previewView;
-    private ImageCapture imageCapture = null;
+    private File file;
+    private final MediaRecorder recorder = null;
+    private final MediaPlayer player = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Initialize Firebase
-        FirebaseApp.initializeApp(this);
-
         setContentView(R.layout.activity_main);
+    }
 
-        previewView = findViewById(R.id.camera_preview);
+    public void onLoadPhotoClick(View view) {
 
-        Button captureButton = findViewById(R.id.capture_button);
-        captureButton.setOnClickListener(v -> takePhoto());
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        if (!hasCameraStoragePermissions()) {
-            ActivityCompat.requestPermissions(this, CAMERA_STORAGE_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+        // Bring up gallery to select a photo
+        startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_PHOTOS);
+
+    }
+
+    public void onLoadVideoClick(View view) {
+
+        // Create intent for picking a video from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+
+        // Bring up gallery to select a video
+        startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_VIDEOS);
+
+    }
+
+    public void onTakePhotoClick(View v) {
+        // Check permissions
+        if (!marshmallowPermission.checkPermissionForCamera()) {
+            marshmallowPermission.requestPermissionForCamera();
         } else {
-            if (!hasLocationPermissions()) {
-                ActivityCompat.requestPermissions(this, LOCATION_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            // create Intent to take a picture and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // set file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+            photoFileName = "IMG_" + timeStamp + ".jpg";
+
+            // Create a photo file reference
+            Uri file_uri = getFileUri(photoFileName, 0);
+
+            // Add extended data to the intent
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file_uri);
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, MY_PERMISSIONS_REQUEST_OPEN_CAMERA);
+            }
+        }
+    }
+
+    public void onRecordVideoClick(View v) {
+        // Check permissions
+        if (!marshmallowPermission.checkPermissionForCamera()) {
+            marshmallowPermission.requestPermissionForCamera();
+        } else {
+            // create Intent to capture a video and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            // set file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+            videoFileName = "VIDEO_" + timeStamp + ".mp4";
+
+            // Create a video file reference
+            Uri file_uri = getFileUri(videoFileName, 1);
+
+            // add extended data to the intent
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file_uri);
+
+            // Start the video record intent to capture video
+            startActivityForResult(intent, MY_PERMISSIONS_REQUEST_RECORD_VIDEO);
+        }
+    }
+
+    // Returns the Uri for a photo/media stored on disk given the fileName
+    public Uri getFileUri(String fileName) {
+        // Get safe storage directory for photos
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.getExternalStorageDirectory().toString()), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+    }
+
+    private void scanFile(String path) {
+
+        MediaScannerConnection.scanFile(MainActivity.this,
+                new String[] { path }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("TAG", "Finished scanning " + path);
+                    }
+                });
+    }
+
+    // Returns the Uri for a photo/media stored on disk given the fileName and type
+    public Uri getFileUri(String fileName, int type) {
+        Uri fileUri = null;
+        try {
+            String typestr = "images"; //default to images type
+            if (type == 1) {
+                typestr = "videos";
+            } else if (type != 0) {
+                typestr = "audios";
+            }
+
+            File mediaStorageDir = new File(getExternalMediaDirs()[0], APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Create the file target for the media based on filename
+            file = new File(mediaStorageDir, fileName);
+
+            // Wrap File object into a content provider, required for API >= 24
+            // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+            if (Build.VERSION.SDK_INT >= 24) {
+                fileUri = FileProvider.getUriForFile(this.getApplicationContext(), "comp5216.sydney.edu.au.mediaapp.fileProvider", file);
             } else {
-                initializeCamera();
+                fileUri = Uri.fromFile(mediaStorageDir);
             }
+        } catch (Exception ex) {
+            Log.e("getFileUri", ex.getStackTrace().toString());
         }
-    }
-
-    private boolean hasCameraStoragePermissions() {
-        for (String permission : CAMERA_STORAGE_PERMISSIONS) {
-            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("MainActivity", "Permission not granted: " + permission);
-                return false;
-            }
-        }
-        Log.d("MainActivity", "Camera and Storage permissions granted");
-        return true;
-    }
-
-    private boolean hasLocationPermissions() {
-        for (String permission : LOCATION_PERMISSIONS) {
-            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
+        return fileUri;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (hasCameraStoragePermissions()) {
-            if (hasLocationPermissions()) {
-                initializeCamera();
-            } else {
-                ActivityCompat.requestPermissions(this, LOCATION_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+        final VideoView mVideoView = findViewById(R.id.videoview);
+        ImageView ivPreview = findViewById(R.id.photopreview);
+
+        mVideoView.setVisibility(View.GONE);
+        ivPreview.setVisibility(View.GONE);
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_OPEN_CAMERA) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+                scanFile(file.getAbsolutePath());
+                ivPreview.setImageBitmap(takenImage);
+                ivPreview.setVisibility(View.VISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            explainPermissionReason("Camera and storage permissions are required to capture and save photos.");
-        }
-    }
-
-    private void explainPermissionReason(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
-
-    private void initializeCamera() {
-        Log.d("MainActivity", "Initializing camera");
-
-        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                if (cameraProvider == null) {
-                    Log.d("MainActivity", "cameraProvider is null");
-                    Toast.makeText(MainActivity.this, "Camera provider is null.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                CameraSelector cameraSelector = new CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build();
-
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_READ_PHOTOS) {
+            if (resultCode == RESULT_OK) {
+                Uri photoUri = data.getData();
+                // Do something with the photo based on Uri
+                Bitmap selectedImage;
                 try {
-                    if (!cameraProvider.hasCamera(cameraSelector)) {
-                        Log.d("MainActivity", "No camera matching the camera selector.");
-                        Toast.makeText(MainActivity.this, "No camera available.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                } catch (CameraInfoUnavailableException e) {
-                    Log.d("MainActivity", "Camera info unavailable: " + e.getMessage());
-                    Toast.makeText(MainActivity.this, "Camera info unavailable.", Toast.LENGTH_LONG).show();
-                    return;
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+
+                    // Load the selected image into a preview
+                    ivPreview.setImageBitmap(selectedImage);
+                    ivPreview.setVisibility(View.VISIBLE);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-
-                Preview preview = new Preview.Builder().build();
-                imageCapture = new ImageCapture.Builder().build();
-                Log.d("MainActivity", "ImageCapture initialized");
-
-                preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-                cameraProvider.unbindAll();
-                cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
-                Log.d("MainActivity", "Camera has been bound to lifecycle");
-
-            } catch (ExecutionException | InterruptedException e) {
-                Toast.makeText(this, "Error initializing camera", Toast.LENGTH_SHORT).show();
-                Log.d("MainActivity", "Error initializing camera: " + e.getMessage());
             }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
-    private void takePhoto() {
-        Log.d("MainActivity", "Inside takePhoto() method");
-
-        if (imageCapture == null) {
-            Toast.makeText(this, "Camera not initialized", Toast.LENGTH_SHORT).show();
-            Log.d("MainActivity", "ImageCapture is null");
-            return;
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_READ_VIDEOS) {
+            if (resultCode == RESULT_OK) {
+                Uri videoUri = data.getData();
+                mVideoView.setVisibility(View.VISIBLE);
+                mVideoView.setVideoURI(videoUri);
+                mVideoView.requestFocus();
+                mVideoView.setOnPreparedListener(new OnPreparedListener() {
+                    // Close the progress bar and play the video
+                    public void onPrepared(MediaPlayer mp) {
+                        mVideoView.start();
+                    }
+                });
+            }
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_RECORD_VIDEO) {
+            //if you are running on emulator remove the if statement
+            if (resultCode != RESULT_OK) {
+                Uri takenVideoUri = getFileUri(videoFileName, 1);
+                mVideoView.setVisibility(View.VISIBLE);
+                mVideoView.setVideoURI(takenVideoUri);
+                mVideoView.requestFocus();
+                mVideoView.setOnPreparedListener(new OnPreparedListener() {
+                    // Close the progress bar and play the video
+                    public void onPrepared(MediaPlayer mp) {
+                        mVideoView.start();
+                    }
+                });
+            }
         }
-
-        File file = new File(getExternalFilesDir(null), "photo-" + System.currentTimeMillis() + ".jpg");
-        if(file.exists()) {
-            Log.d("MainActivity", "File already exists");
-        } else {
-            Log.d("MainActivity", "New file created at: " + file.getAbsolutePath());
-        }
-
-        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
-
-        Log.d("MainActivity", "About to capture image");
-
-        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
-            @Override
-            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                String msg = "Photo captured: " + outputFileResults.getSavedUri();
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                Log.d("MainActivity", "Image saved successfully");
-            }
-
-            @Override
-            public void onError(@NonNull ImageCaptureException exception) {
-                Toast.makeText(getApplicationContext(), "Error capturing photo", Toast.LENGTH_SHORT).show();
-                Log.e("MainActivity", "Image capture error: " + exception.getMessage(), exception);
-            }
-        });
     }
-
 }
